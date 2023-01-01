7d5ca5ddb63ae39a47e66c60863a94ec5786af8e
aniket-vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-01-01 22:51:42 +0530

    Create bm_1_andBitMagic.java

---
+++ 2. Bit Magic/bm_1_andBitMagic.java
@@ -0,0 +1,17 @@
+//  A N D ( & )
+
+public class bm_1_andBitMagic {
+	public static void main(String[] args) {
+		int x = 3 , y = 6;
+		System.out.println(x & y);
+	}
+}
+
+// x = 3    0....0011   (in binary)
+// y = 6    0....0110
+//---------------------
+// x & y    0....0010   (in decimal : 2)
+//---------------------
+
+
+// OUTPUT :- 2
