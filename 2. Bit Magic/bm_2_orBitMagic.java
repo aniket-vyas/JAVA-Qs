def5f20d3f98dc86de00c83b08509358e6a03506
aniket-vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-01-02 23:22:18 +0530

    Create bm_2_orBitMagic.java

---
+++ 2. Bit Magic/bm_2_orBitMagic.java
@@ -0,0 +1,16 @@
+// OR Bit Magic
+
+public class bm_2_orBitMagic{
+  public static void main(String[] args) {
+		int x = 3 , y = 6;
+		System.out.println(x | y);
+	}
+}
+
+// x     : 0....0011  (3 in binary)
+// y     : 0....0110  (6 in binary)
+//--------------------
+// x | y : 0....0111  (7 in decimal)
+//--------------------
+
+// OUTPUT : 7
