1d58cf47f4bf2244ca16e1672331a22781a8376e
aniket-vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-01-02 23:17:04 +0530

    Update bm_3_xorBitMagic.java

--- 2. Bit Magic/bm_3_xorBitMagic.java
+++ 2. Bit Magic/bm_3_xorBitMagic.java
@@ -6,3 +6,11 @@ public class xorbitmagic {
 		System.out.println(x ^ y);
 	}
 }
+
+// x 	 : 0....0011	(in binary)
+// y 	 : 0....0110
+//--------------------
+// x ^ y : 0....0101	(5 in decimal)
+//--------------------
+
+// OUTPUT : 5
