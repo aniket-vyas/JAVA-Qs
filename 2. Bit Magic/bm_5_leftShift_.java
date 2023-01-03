76a405d010e6528e1e5690b1270bbf3c50dbbca5
Aniket Vyas <121762607+aniket-vyas@users.noreply.github.com>
2023-01-03 23:23:09 +0530

    Create bm_5_leftShift_.java

---
+++ 2. Bit Magic/bm_5_leftShift_.java
@@ -0,0 +1,19 @@
+// Left Shift 
+
+public class leftShift {
+	public static void main(String[] args) {
+		int x = 3;
+		System.out.println(x << 1);   // OUTPUT : 6
+		System.out.println(x << 2);   // OUTPUT : 12
+		int y = 4;
+		int z = (x << y);
+		System.out.println(z);        // OUTPUT : 48
+	}
+}
+
+
+// Logic :
+//  int x= 3 : 000.......0011
+//  (x << 1) : 000.......0110
+//  (x << 2) : 000......01100
+//  (x << 4) : 000....0110000
